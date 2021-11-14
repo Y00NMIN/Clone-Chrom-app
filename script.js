a4ac026e9913c78ad61abbf608f1f644885e9ead
Sungmin Yoon <89017779+Y00NMIN@users.noreply.github.com>
2021-11-14 15:01:42 +0000

    object nico

---
+++ script.js
@@ -0,0 +1,21 @@
+const amIFat = true;
+const amITall = false;
+//null 아무것도 없음
+//false 값이 존재함, 거짓이란 값
+//
+let something 
+console.log(amIFat);
+
+
+const player ={
+  name:"nico",
+  points: 10,
+  fat:true,
+  handsome:false,
+};
+
+console.log(player);
+player.fat = false;
+console.log(player);
+player.lastName= "yoon";
+console.log(player);
