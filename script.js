ee641b1ebc06b503bfd551c1106fd091713e907b
Sungmin Yoon <89017779+Y00NMIN@users.noreply.github.com>
2021-11-15 14:54:38 +0000

    if nico

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-const amIFat = true;
-const amITall = false;
-//null 아무것도 없음
-//false 값이 존재함, 거짓이란 값
-//
-let something 
-console.log(amIFat);
 
+const age = parseInt(prompt("How old are you"));
 
-const player ={
-  name:"nico",
-  points: 10,
-  fat:true,
-  handsome:false,
-};
-
-console.log(player);
-player.fat = false;
-console.log(player);
-player.lastName= "yoon";
-console.log(player);
+if(isNaN(age)){
+  return ("Please write a number");
+}else{
+  return ("Thx"); 
+}
