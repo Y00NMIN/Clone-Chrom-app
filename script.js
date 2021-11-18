90348001896d78a54aa8c5294fb167be67bef70a
Sungmin Yoon <89017779+Y00NMIN@users.noreply.github.com>
2021-11-18 14:23:34 +0000

    event

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const title = document.querySelector("div.title:first-child h1");
+const h1 = document.querySelector("div.title:first-child h1");
 
 function handleTitleClick(){
-  title.style.color = "blue";
+  const currentColor =h1.style.color;
+  let newColor ;
+  if(h1.style.color === "blue"){
+    newColor = "tomato";
+  }else{
+   newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter(){
+  h1.style.color = "red";
+}
+
+function handleMouseLeave(){
+  h1.style.color = "blue";
+}
+
+function handleWindowResize(){
+  document.body.style.backgroundColor = "tomato";
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave );
+
