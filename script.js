c3419e253a21174c37049b81d999164c7fe5dfca
Sungmin Yoon <89017779+Y00NMIN@users.noreply.github.com>
2021-11-19 14:59:54 +0000

    toggle 이해

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
 const h1 = document.querySelector("div.title:first-child h1");
 
 function handleTitleClick(){
-  const currentColor =h1.style.color;
-  let newColor ;
-  if(h1.style.color === "blue"){
-    newColor = "tomato";
-  }else{
-   newColor = "blue";
-  }
-  h1.style.color = newColor;
-}
-
-function handleMouseEnter(){
-  h1.style.color = "red";
-}
-
-function handleMouseLeave(){
-  h1.style.color = "blue";
-}
-
-function handleWindowResize(){
-  document.body.style.backgroundColor = "tomato";
+  h1.classList.toggle("active");
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave );
-
