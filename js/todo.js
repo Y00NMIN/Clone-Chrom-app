2eaa4a39805c395b04f509b39dc86c512e6a4b64
Sungmin Yoon <89017779+Y00NMIN@users.noreply.github.com>
2021-11-25 14:16:13 +0000

    Saving To Dos localStorage. save

--- js/todo.js
+++ js/todo.js
@@ -1,14 +1,31 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoInput = toDoForm.querySelector("input");
 const toDoiList = document.getElementById("todo-list");
+
+const toDos = []
+
+function saveToDo(){
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteToDo(event){
+  const li = event.target.parentElement;
+  //parentElement은 elemnt의 부모님
+  li.remove();
+}
 
 function paintToDo(newToDo){
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
-  //li안에 span추가
   span.innerText = newToDo;
   //텍스트를 span내부에 추가
+  const button = document.createElement("button");
+
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  //li안에 span추가
   toDoiList.appendChild(li);
 }
 
@@ -16,7 +33,9 @@ function handleToDoSubmit(event){
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newToDo);
   paintToDo(newToDo);
+  saveToDo();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
