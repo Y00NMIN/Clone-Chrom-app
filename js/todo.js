58742264f92d991bc8f671193531179d8dbb09df
Sungmin Yoon <89017779+Y00NMIN@users.noreply.github.com>
2021-11-26 14:47:52 +0000

    todo section end

--- js/todo.js
+++ js/todo.js
@@ -2,22 +2,27 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoiList = document.getElementById("todo-list");
 
-const toDos = []
+const TODOS_KEY ="todos";
+
+let toDos = [];
 
 function saveToDo(){
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event){
   const li = event.target.parentElement;
   //parentElement은 elemnt의 부모님
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDo();
 }
 
 function paintToDo(newToDo){
   const li = document.createElement("li");
+  li.id = newToDo.id;
   const span = document.createElement("span");
-  span.innerText = newToDo;
+  span.innerText = newToDo.text;
   //텍스트를 span내부에 추가
   const button = document.createElement("button");
 
@@ -33,9 +38,22 @@ function handleToDoSubmit(event){
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newToDo);
-  paintToDo(newToDo);
+  const newToDoObj = {
+    text: newToDo,
+    id: Date.now(),
+  };
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
   saveToDo();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
+
